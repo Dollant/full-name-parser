e4cd6e1548af30d406c77435cb660a85f7d07dfb
Dollant <ariawilliams2425@gmail.com>
2026-04-13 13:56:19 -0400

    Added prompt user for input, trim whitespace, and guard against empty input.

--- src/main/java/com/pluralsight/FullNameParser.java
+++ src/main/java/com/pluralsight/FullNameParser.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class FullNameParser {
+
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter your name: ");
+        String input = keyboard.nextLine()  .trim();
+
+        if (input.isEmpty()) {
+            System.out.println("No name was entered. Please restart. ^_^");
+            keyboard.close();
+            return;
+        }
+    }
 }
